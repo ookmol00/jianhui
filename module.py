518be7a5b5838722f3e1b176fd7d4c19a08b6060
jianhui <ookmol00@gmail.com>
2020-04-05 11:48:37 +0800

    feat: Add module import example

---
+++ module.py
@@ -0,0 +1,13 @@
+# grades=[1,2,3,4,5]
+# # print(grades)
+# def power(base,exp=0):
+# #     print(base*exp)
+# import sys
+# print(sys.maxsize)
+# import geometry
+# result=geometry.slope(2,4,6,8)
+# print(result)
+# result=geometry.distance(5,5,9,9)
+# print(result)
+import sys
+print(sys.path)
